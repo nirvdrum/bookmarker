4382cb2c1f796e9436e5c492820455d38bffefb4
Kevin Menard <nirvdrum@gmail.com>
2004-09-23 04:05:57 +0000

    Added a new exception page that helps pinpoint the problem.  Also updated a number of third party jars.

---
+++ src/net/negativetwenty/bookmarker/EnhancedEngine.java
@@ -0,0 +1,23 @@
+/*
+ * Created on Sep 23, 2004
+ *
+ * TODO To change the template for this generated file go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+package net.negativetwenty.bookmarker;
+
+import org.apache.tapestry.engine.BaseEngine;
+
+/**
+ * @author nirvdrum
+ *
+ * TODO To change the template for this generated type comment go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+public class EnhancedEngine extends BaseEngine
+{
+    protected String getExceptionPageName() 
+    {
+       return "exception:Exception";
+    }
+}
