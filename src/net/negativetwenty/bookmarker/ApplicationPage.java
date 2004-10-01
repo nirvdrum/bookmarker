5fdd21f93ee163dfbed8615a828857e72fcbb109
Kevin Menard <nirvdrum@gmail.com>
2004-10-01 20:45:19 +0000

    Starting to move to a pure CSS layout.

--- src/net/negativetwenty/bookmarker/ApplicationPage.java
+++ src/net/negativetwenty/bookmarker/ApplicationPage.java
@@ -6,8 +6,10 @@
  */
 package net.negativetwenty.bookmarker;
 
+import java.net.URL;
 import java.util.*;
 
+import net.negativetwenty.bookmarker.models.Bookmark;
 import net.negativetwenty.bookmarker.models.Category;
 
 import org.apache.tapestry.*;
@@ -16,6 +18,14 @@ import org.apache.tapestry.event.*;
 import org.apache.tapestry.html.BasePage;
 import org.objectstyle.cayenne.access.DataContext;
 import org.objectstyle.cayenne.query.SelectQuery;
+
+import de.nava.informa.core.CategoryIF;
+import de.nava.informa.core.ChannelBuilderIF;
+import de.nava.informa.core.ChannelExporterIF;
+import de.nava.informa.core.ChannelIF;
+import de.nava.informa.exporters.*;
+import de.nava.informa.impl.basic.ChannelBuilder;
+import de.nava.informa.impl.basic.Item;
 
 /**
  * A superclass of all application pages. Contains utility methods
@@ -67,5 +77,50 @@ public class ApplicationPage extends BasePage implements PageRenderListener, ITr
 		        throw new PageRedirectException("ViewBookmarks");
 		    }
 		}
+		
+		createRdf();
+	}
+	
+	public void createRdf()
+	{
+	    DataContext dc = getDataContext();
+        SelectQuery query = new SelectQuery(Bookmark.class);
+        List bookmarks = getDataContext().performQuery(query); 
+	    
+	    try
+	    {
+	        ChannelBuilderIF builder = new ChannelBuilder();
+	        ChannelIF channel = builder.createChannel("Bookmarks");
+	        channel.setDescription("Test Channel: " + "Bookmarks");
+	        
+	        Iterator it = bookmarks.iterator();
+	        while (it.hasNext())
+	        {
+	            Bookmark b = (Bookmark) it.next();
+	            
+	            Item item = new Item(b.getTitle(), b.getDescription(), new URL(b.getUrl()));
+	            
+	            // TODO When I finally force Bookmarks to have some category, the category name should never be null.
+	            if (b.getCategory() != null)
+	            {
+	                CategoryIF category = new de.nava.informa.impl.basic.Category(b.getCategory().getName());
+	                channel.addCategory(category);
+	                item.addCategory(category);
+	            }
+	            
+	            // TODO Update this to whatever the logged in username is.
+	            item.setCreator("nirvdrum");
+	            
+	            channel.addItem(item);
+	        }
+
+	        String rdffile = getRequestCycle().getRequestContext().getServlet().getServletContext().getRealPath(getComponent("border").getAsset("rdffile").getResourceLocation().getPath());
+	        ChannelExporterIF exporter = new RSS_1_0_Exporter(rdffile);
+	        exporter.write(channel);
+	    }
+	    catch (Exception e)
+	    {
+	        e.printStackTrace();
+	    }
 	}
 }
