453ef9e4d73d164b252d34eb8a8e9347f2f4b351
Kevin Menard <nirvdrum@gmail.com>
2004-11-19 15:11:56 +0000

    Added some comments.  Correctly set an item's author in the RDF file.  Don't deploy by default in the ant build file (this way deploying to Tomcat is optional, not required).

--- src/net/negativetwenty/bookmarker/AddBookmark.java
+++ src/net/negativetwenty/bookmarker/AddBookmark.java
@@ -56,26 +56,36 @@ public abstract class AddBookmark extends SecureApplicationPage
 		return new CategorySelectionModel(getDataContext());
 	}
 	
+	// TODO This should be refactored to be a different name, since it is also used to modify existing bookmarks.
 	public void addBookmark(IRequestCycle cycle)
 	{
 		DataContext dc = getDataContext();
 		Bookmark b = getBookmark();
 		
+		// If this is a new bookmark (an existing one -- indicating a modification -- would not have a null objectID) . . .
 		if (b.getObjectId() == null)
 		{
+		    // register the object with the data context . . .
 		    dc.registerNewObject(b);
 		    
+		    // and initialize some parameters.
 		    b.setClickCount(new Integer(0));
 		    b.setCreated(new Date());
 		    b.setCreatedBy(getUser());
 		}
 		
+		// Set the bookmark's category and commit it to the db.
 		b.setCategory(getCategory());
 		dc.commitChanges();
+		
+		// Reset the bookmark object so future requests don't think we're trying to modify an existing bookmark.
 		setBookmark(null);
 		
+		// Update the RDF file.
 		createRdf();
 		
+		// TODO This should probably be a callback, so one could add multiple bookmarks to the same category without a bunch of clicking.
+		// Go back to the home page.
 	    cycle.activate("Home");
 	}
 	
@@ -83,17 +93,23 @@ public abstract class AddBookmark extends SecureApplicationPage
 	{	
 	    Visit v = (Visit) getVisit();
 	    
+	    // If the bookmark object is null, create a new one.
 		if (getBookmark() == null)
 		{
 			setBookmark(new Bookmark());
 		}
 		
+		// If the category object is null . . .
 		if (getCategory() == null)
 		{
+		    // try to set it to the category stored in the visit object.
+		    // This corresponds to clicking "Add Bookmark" from a page with a list of a given category's bookmarks.
 		    if (v.getCategory() != null)
 		    {
 		        setCategory(v.getCategory());
 		    }
+		    
+		    // Otherwise, just give it a new category.  This will show as "N/A" in the category selection list in the HTML page.
 		    else
 		    {
 		        setCategory(new Category());
@@ -103,23 +119,29 @@ public abstract class AddBookmark extends SecureApplicationPage
 	
 	public void createRdf()
 	{
+	    // Get a list of all the bookmarks.
         SelectQuery query = new SelectQuery(Bookmark.class);
-        List bookmarks = getDataContext().performQuery(query); 
+        List bookmarks = getDataContext().performQuery(query);
+        Visit v = (Visit) getVisit();
 	    
 	    try
 	    {
+	        // Set things up for creating the RDF file.
 	        ChannelBuilderIF builder = new ChannelBuilder();
 	        ChannelIF channel = builder.createChannel("Bookmarks");
 	        channel.setDescription("Test Channel: " + "Bookmarks");
 	        
+	        // For each bookmark . . .
 	        Iterator it = bookmarks.iterator();
 	        while (it.hasNext())
 	        {
 	            Bookmark b = (Bookmark) it.next();
 	            
+	            // create an item for the RDF file . . .
 	            Item item = new Item(b.getTitle(), b.getDescription(), new URL(b.getUrl()));
 	            
 	            // TODO When I finally force Bookmarks to have some category, the category name should never be null.
+	            // if the bookmark has an associated category, create that category for the item in RDF . . .
 	            if (b.getCategory() != null)
 	            {
 	                CategoryIF category = new de.nava.informa.impl.basic.Category(b.getCategory().getName());
@@ -127,12 +149,12 @@ public abstract class AddBookmark extends SecureApplicationPage
 	                item.addCategory(category);
 	            }
 	            
-	            // TODO Update this to whatever the logged in username is.
-	            item.setCreator("nirvdrum");
-	            
+	            // indicate the creator of the item and then add it to the RDF file.
+	            item.setCreator(v.getUser().getUsername());
 	            channel.addItem(item);
 	        }
 
+	        // Actually create the RDF file now.
 	        String rdffile = getRequestCycle().getRequestContext().getServlet().getServletContext().getRealPath(getComponent("border").getAsset("rdffile").getResourceLocation().getPath());
 	        ChannelExporterIF exporter = new RSS_2_0_Exporter(rdffile);
 	        exporter.write(channel);
